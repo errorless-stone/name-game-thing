c43fa1f03905a92853ec990824821b05bdb91034
Sutton <asutton4@una.edu>
2023-10-31 13:50:17 -0500

    changed directory stuffs

--- public/scripts/nameGameScript.js
+++ public/scripts/nameGameScript.js
@@ -1,3 +1,20 @@
 $(function() {
     console.log( "ready!" );
+
+    // var randName = Math.floor(Math.random() * profileData.length());
+
+
+
+       //const clientData= <%=profileData %>
+
+    $("#pic").on("click", () => {
+
+        console.log("clicked");
+        console.log("clientData[1]");
+
+        
+    })
 });
+
+
+
