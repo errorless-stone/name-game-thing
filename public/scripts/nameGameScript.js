232540af0a81779843dfc684a53b6da3c0e9f71b
Barry <barrycumbie@gmail.com>
2023-10-31 16:39:14 -0500

    got it going

--- public/scripts/nameGameScript.js
+++ public/scripts/nameGameScript.js
@@ -1,3 +1,14 @@
 $(function() {
     console.log( "ready!" );
+
+    $("#pic").on("click", (e)=>{
+        console.log("clicky");
+        $("#pic").attr("src", clientData[Math.floor(Math.random()*clientData.length)].image)
+    }); 
+
+       
+    $.each(clientData, (key, value) => {
+    //   console.log(key, value);
+    //   console.log(value.image); 
+    })
 });
