4ce0f0ca731eca4cb662f45aaf2adaace72ee38c
Sutton <asutton4@una.edu>
2023-11-09 13:45:08 -0600

    got buttons to change pic

--- public/scripts/nameGameScript.js
+++ public/scripts/nameGameScript.js
@@ -1,11 +1,33 @@
+
+
 $(function() {
     console.log( "ready!" );
 
-    $("#pic").on("click", (e)=>{
-        console.log("clicky");
-        $("#pic").attr("src", clientData[Math.floor(Math.random()*clientData.length)].image)
-    }); 
+    var randProf = clientData[Math.floor(Math.random()*clientData.length)];
 
+
+    //load three buttons. id=but1 but2 but3 
+
+    $("#pic").attr("src", randProf.image); 
+    var randProfName = randProf.name; 
+
+    $("button").on("click", (e)=>{
+        console.log("clicky");
+        $("#pic").fadeOut(1000,function(){
+            $("#pic").attr("src", randProf.image); 
+        });
+        
+        randProfName = randProf.name;
+
+        console.log("correct person", randProfName); 
+        console.log("name of button we clicked", $("#aBUTTON").text()); 
+
+
+        //if(randProfName === $(this).valattr("innnerHTML") {}
+
+        $("#pic").fadeIn();
+        randProf = clientData[Math.floor(Math.random()*clientData.length)];
+    }); 
 
     $.each(clientData, (key, value) => {
     //   console.log(key, value);
