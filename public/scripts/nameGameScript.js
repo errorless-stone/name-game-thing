82a175965c6047ce2b6ff6bb11bb8aefa2e03e6a
Sutton <asutton4@una.edu>
2023-11-14 13:55:34 -0600

    stuffs

--- public/scripts/nameGameScript.js
+++ public/scripts/nameGameScript.js
@@ -3,35 +3,54 @@
 $(function() {
     console.log( "ready!" );
 
-    var randProf = clientData[Math.floor(Math.random()*clientData.length)];
-
+    var rightProf = clientData[Math.floor(Math.random()*clientData.length)];
+    var wrongProf = clientData[Math.floor(Math.random()*clientData.length)];
+    var wrongProf2 = clientData[Math.floor(Math.random()*clientData.length)];
+    var rightProfName = rightProf.name;
+    var wrongProfName = wrongProf.name;
+    var wrongProfName2 = wrongProf2.name;
+    var randArray = [rightProfName, wrongProfName, wrongProfName2];
 
     //load three buttons. id=but1 but2 but3 
+    $(document).ready(function() {
+        for(i=0; i < 3; i++){
+            $('#button-box').append(
+                $(document.createElement('button')).prop({
+                    type: 'button',
+                    innerHTML: randArray[i],
+                    class: 'btn btn-primary guessButton',
+                    id: "button"+(i+1),
+                    style: "margin-right: 20px"
+                })
+            ); 
+        }
+    });
 
-    $("#pic").attr("src", randProf.image); 
-    var randProfName = randProf.name; 
+    console.log("rightProfName",rightProfName)
+
+    $("#pic").attr("src", rightProf.image); 
 
     $("button").on("click", (e)=>{
         console.log("clicky");
         $("#pic").fadeOut(1000,function(){
-            $("#pic").attr("src", randProf.image); 
+            $("#pic").attr("src", rightProf.image); 
         });
         
-        randProfName = randProf.name;
+        rightProfName = rightProf.name;
 
-        console.log("correct person", randProfName); 
+        console.log("correct person", rightProfName); 
         console.log("name of button we clicked", $("#aBUTTON").text()); 
 
 
         //if(randProfName === $(this).valattr("innnerHTML") {}
 
         $("#pic").fadeIn();
-        randProf = clientData[Math.floor(Math.random()*clientData.length)];
+        rightProf = clientData[Math.floor(Math.random()*clientData.length)];
     }); 
 
     $.each(clientData, (key, value) => {
-    //   console.log(key, value);
-    //   console.log(value.image); 
+      console.log(key, value);
+      console.log(value.image); 
     })
 });
 
