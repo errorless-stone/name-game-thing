a51926b620bcd6c1b8bebfd286e755c26740095a
Sutton <asutton4@una.edu>
2023-11-16 13:53:05 -0600

    We did .... something

--- public/scripts/nameGameScript.js
+++ public/scripts/nameGameScript.js
@@ -3,19 +3,27 @@
 $(function() {
     console.log( "ready!" );
 
+    // var rightProf = "CORRECT ONE";
+    var wrongProf = "NOT 1";
+    var wrongProf2 = "NOT 2";
     var rightProf = clientData[Math.floor(Math.random()*clientData.length)];
-    var wrongProf = clientData[Math.floor(Math.random()*clientData.length)];
-    var wrongProf2 = clientData[Math.floor(Math.random()*clientData.length)];
+    // var wrongProf = clientData[Math.floor(Math.random()*clientData.length)];
+        // var wrongProf2 = clientData[Math.floor(Math.random()*clientData.length)];
+    // while(wrongProf2 == rightProf || wrongProf2 == wrongProf){
+    //     wrongProf2 = clientData[Math.floor(Math.random()*clientData.length)];
+    // }
     var rightProfName = rightProf.name;
     var wrongProfName = wrongProf.name;
     var wrongProfName2 = wrongProf2.name;
-    var randArray = [rightProfName, wrongProfName, wrongProfName2];
+    //var randArray = [rightProfName, wrongProfName, wrongProfName2];
+    var randArray = [rightProfName, wrongProf, wrongProf2];
 
     //load three buttons. id=but1 but2 but3 
      for(i=0; i < 3; i++){
         $('#button-box').append(
             $(document.createElement('button')).prop({
                 type: 'button',
+                value: randArray[i], 
                 innerHTML: randArray[i],
                 class: 'btn btn-primary guessButton',
                 id: "button"+(i+1),
@@ -28,27 +36,54 @@ $(function() {
 
     $("#pic").attr("src", rightProf.image); 
 
-    $(".guessButton").on("click", (e)=>{
-        console.log("clicky");
-        $("#pic").fadeOut(1000,function(){
-            $("#pic").attr("src", rightProf.image); 
-        });
-        
-        rightProfName = rightProf.name;
 
-        console.log("correct person", rightProfName); 
-        console.log("name of button we clicked", $("#aBUTTON").text()); 
+    
+    // $("#button2").on("click", (e)=>
+    // {
+    //     checkAnswer("#button2"); 
+    // }); 
+
+    // $("#button3").on("click", (e)=>
+    // {
+    //     checkAnswer("#button3"); 
+    // }); 
+
+
+ // function checkAnswer(btnId)
+ $("button1").on("click", (e)=>
+  {
+        console.log("clicky");
+        console.log("PROF:", rightProfName);
+        console.log("btnclicked val", $(this).text());
+
+        var pickedProf = $("#button1").text();
+        if(rightProfName ==  pickedProf){
+
+            console.log("correct AND now reload the data, mathr floros");
+        }
+        else {
+            console.log("not correct")
+        }
+
+        // $("#pic").fadeOut(1000,function(){
+        //     $("#pic").attr("src", rightProf.image); 
+        // });
+        
+        // rightProfName = rightProf.name;
+
+        // console.log("correct person", rightProfName); 
+        // console.log("name of button we clicked", $("#aBUTTON").text()); 
 
 
         //if(randProfName === $(this).valattr("innnerHTML") {}
 
         $("#pic").fadeIn();
-        rightProf = clientData[Math.floor(Math.random()*clientData.length)];
+        // rightProf = clientData[Math.floor(Math.random()*clientData.length)];
     }); 
 
     $.each(clientData, (key, value) => {
-      console.log(key, value);
-      console.log(value.image); 
+    //   console.log(key, value);
+    //   console.log(value.image); 
     })
 });
 
